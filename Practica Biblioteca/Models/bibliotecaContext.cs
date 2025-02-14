973d1d3c3d8a01fc639edd6a41e3a85e5de45c3f
WWarrior64 <115961340+WWarrior64@users.noreply.github.com>
2025-02-13 19:05:41 -0600

    Primera parte de la practica, creacion del contexto y la configuracion de la conexion

---
+++ Practica Biblioteca/Models/bibliotecaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Practica_Biblioteca.Models
+{
+	public class bibliotecaContext : DbContext
+	{
+		public bibliotecaContext(DbContextOptions<bibliotecaContext> options) : base(options) 
+		{
+			
+		}
+	}
+}
--- Practica Biblioteca/Program.cs
+++ Practica Biblioteca/Program.cs
@@ -1,8 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Practica_Biblioteca.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//Inyeccion por dependencias del string de conexion al contexto
+builder.Services.AddDbContext<bibliotecaContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("bibliotecaDbConnection")
+	)
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
